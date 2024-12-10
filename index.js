9022b3c19bbb9ddb26257b15bda88973f5eb3419
Srikanth <134727297+srikanth-nuthalapati@users.noreply.github.com>
2024-12-10 10:23:49 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const fs = require("fs");
+const cors = require("cors")
+
+app.use(express.json())
+app.use(cors())
+
+app.get("/menu",(req,res)=>{
+    fs.readFile("day13/db.json",(err,data)=>{
+        if(err) {
+            res.send("error",err.message);
+        }
+        else{
+            res.send(data)  
+        }
+    });
+}).listen(3000,()=>{
+    console.log("server is running on port 3000");
+})
