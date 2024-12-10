830d1e74654b538080c1085593f167f185046e8f
Srikanth <134727297+srikanth-nuthalapati@users.noreply.github.com>
2024-12-10 10:41:37 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require("express");
 const app = express();
 const fs = require("fs");
 const cors = require("cors")
+const path = require("path");
 
 app.use(express.json())
 app.use(cors())
 
 app.get("/menu",(req,res)=>{
-    fs.readFile("day13/db.json",(err,data)=>{
+    const filePath = path.join(__dirname,"menu.json");
+    fs.readFile(filePath,(err,data)=>{
         if(err) {
             res.send(err.message);
         }
